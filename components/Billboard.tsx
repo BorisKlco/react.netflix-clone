97adff60c459a049f1f4f2a5490d9a6d58999da6
Boris Klco <klco@pm.me>
2023-04-25 11:47:29 +0000

    adding info card

--- components/Billboard.tsx
+++ components/Billboard.tsx
@@ -1,10 +1,17 @@
 import useBillboard from "@/hooks/useBillboard";
 
+import useInfoModalStore from "@/hooks/useInfoModalStore";
+import { useCallback } from "react";
 import { BsInfoSquare } from "react-icons/bs";
 import PlayButton from "./PlayButton";
 
 const Billboard = () => {
+  const { openModal } = useInfoModalStore();
   const { data } = useBillboard();
+
+  const handleOpenModal = useCallback(() => {
+    openModal(data?.id);
+  }, [openModal, data?.id]);
 
   return (
     <div className="relative h-[56.25vw]">
@@ -30,7 +37,10 @@ const Billboard = () => {
         </p>
         <div className="flex flex-row items-center mt-3 md:mt-4 gap-3">
           <PlayButton movieId={data?.id} />
-          <button className="bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-opacity-20 transition">
+          <button
+            onClick={handleOpenModal}
+            className="bg-white text-white bg-opacity-30 rounded-md py-1 md:py-2 px-2 md:px4 w-auto text-xs lg:text-lg font-semibold flex flex-row items-center hover:bg-opacity-20 transition"
+          >
             <BsInfoSquare className="mr-1" />
             More Info
           </button>
--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,7 @@
+import useInfoModalStore from "@/hooks/useInfoModalStore";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useCallback } from "react";
+import { AiOutlineInfo } from "react-icons/ai";
 import { BiPlay } from "react-icons/bi";
 import FavoriteButton from "./FavoriteButton";
 
@@ -9,9 +11,19 @@ interface MovieCardProps {
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const router = useRouter();
+
+  const { openModal } = useInfoModalStore();
+
+  const redirectToWatch = useCallback(
+    () => router.push(`/watch/${data.id}`),
+    [router, data.id]
+  );
+
   return (
     <div className="group bg-zinc-900 col-span relative h-[12vw]">
       <img
+        onClick={redirectToWatch}
+        draggable={false}
         className="
         cursor-pointer
         object-cover
@@ -48,6 +60,8 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
       "
       >
         <img
+          onClick={redirectToWatch}
+          draggable={false}
           className="
          cursor-pointer
          object-cover
@@ -90,11 +104,17 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
                 transition
                 hover:bg-neutral-300
                 "
-              onClick={() => router.push(`/watch/${data?.id}`)}
+              onClick={redirectToWatch}
             >
               <BiPlay size={30} />
             </div>
             <FavoriteButton movieId={data?.id} />
+            <div
+              onClick={() => openModal(data?.id)}
+              className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300"
+            >
+              <AiOutlineInfo className="text-white group-hover/item:text-neutral-300 w-4 lg:w-6" />
+            </div>
           </div>
           <p className="text-green-400 font-semibold mt-4">
             New <span className="text-white">2023</span>
